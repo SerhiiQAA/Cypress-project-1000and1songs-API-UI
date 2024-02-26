db58b27258a066fa3057d2312170661742b44ae2
SerhiiQA <nabukve@gmail.com>
2024-02-26 13:30:12 +0200

    dsg

--- cypress/e2e/API/Expedition/Get Expeditions List By Category.cy.js
+++ cypress/e2e/API/Expedition/Get Expeditions List By Category.cy.js
@@ -1,54 +1,20 @@
-describe('Get Expedition', () => {
-    it('Метод GET повинен повертати відповідь з обовязковими полями', () => {
-        const itemId = 1; // Ваш ID для пошуку
-    
-        cy.request({
-          method: 'GET',
-          url: `https://1000and1songs.com/api/v1/expedition/${itemId}`,
-          // Додайте інші параметри запиту, якщо потрібно
-        }).then((response) => {
-          expect(response).to.have.property('status', 200);
-          expect(response.body).to.not.be.null;
-    
-          // Перевірка наявності обов'язкових ключів в відповіді
-          expect(response.body).to.have.all.keys(
-            // 'id',
-            // // 'title',
-            // // 'short_description',
-            // // 'preview_photo',
-            // // 'location',
-            // // 'expedition_date'
-          );
-        });
-    });
+describe('API Tests', () => {
+  it('should return valid response from the API', () => {
+    cy.request('GET', 'https://1000and1songs.com/api/v1/expedition/filter') // Replace with your actual API endpoint
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.not.be.null
+        expect(response.body).to.have.property('items').that.is.an('array');
+        expect(response.body.items[0]).to.have.property('id').that.is.a('number');
+        expect(response.body.items[0]).to.have.property('title').that.is.a('string');
+        expect(response.body.items[0]).to.have.property('location').that.is.a('string');
+        expect(response.body.items[0]).to.have.property('short_description').that.is.a('string');
+        expect(response.body.items[0]).to.have.property('expedition_date').that.matches(/\d{4}-\d{2}-\d{2}/);
+        expect(response.body.items[0]).to.have.property('preview_photo').that.is.a('string');
+        expect(response.body).to.have.property('total').that.is.a('number');
+        expect(response.body).to.have.property('page').that.is.a('number');
+        expect(response.body).to.have.property('size').that.is.a('number');
+        expect(response.body).to.have.property('pages').that.is.a('number');
+      });
+  });
 });
-
-// describe('Get One News', () => {
-//     it('Метод GET повинен повертати відповідь з обов\'язковими полями', () => {
-//       const itemId = 1; // Ваш ID для пошуку
-  
-//       cy.request({
-//         method: 'GET',
-//         url: `https://1000and1songs.com/api/v1/news/news/${itemId}`,
-//         // Додайте інші параметри запиту, якщо потрібно
-//       }).then((response) => {
-//         expect(response).to.have.property('status', 200);
-//         expect(response.body).to.not.be.null;
-  
-//         // Перевірка наявності обов'язкових ключів в відповіді
-//         expect(response.body).to.have.all.keys(
-//           'id',
-//           'title',
-//           'short_description',
-//           'preview_photo',
-//           'created_at',
-//           'category',
-//           'location',
-//           'content',
-//           'authors',
-//           'editors',
-//           'photographers'
-//         );
-//       });
-//     });
-//   });
