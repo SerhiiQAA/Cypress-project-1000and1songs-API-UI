e69d7746cdc15b69f1017daca47a30e087315b43
SerhiiQA <nabukve@gmail.com>
2024-03-18 22:47:57 +0200

    new

--- cypress/e2e/API/Get_Team.cy.js
+++ cypress/e2e/API/Get_Team.cy.js
@@ -1,12 +1,19 @@
 describe('Get Team', () => {
-    it('method get response code should be  200', () => {
-      cy.request('https://1000and1songs.com/api/v1/team').then(response => {
-        expect(response).to.have.property('status',200)
-        expect(response.body).to.not.be.null
-        response.body.forEach((partner) => {
-            expect(partner).to.have.all.keys('id', "full_name", 'photo', "description");
-          });
-            // expect(response.body).to.have.length(3);
-        })
-      }) 
-    })
+  it('should validate the response body', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://1000and1songs.com/api/v1/team',
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('array').that.is.not.empty;
+
+      // Checking each object in an array
+      response.body.forEach((item) => {
+        expect(item).to.have.property('id').that.is.a('number');
+        expect(item).to.have.property('full_name').that.is.a('string');
+        expect(item).to.have.property('photo').that.is.a('string').and.matches(/^https:\/\/.*$/);
+        expect(item).to.have.property('description').that.is.a('string');
+      });
+    });
+  });
+});
