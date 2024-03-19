940f54499764f681040614435399aaea27b82433
SerhiiQA <nabukve@gmail.com>
2024-03-19 21:33:02 +0200

    new

--- cypress/e2e/Front/Filter_Map_page.cy.js
+++ cypress/e2e/Front/Filter_Map_page.cy.js
@@ -17,16 +17,21 @@ describe('Support', () => {
 // Check the search by song title
         cy.get('.song__card__header').should('be.visible')
         .contains('Зозуленька')
-
+// Check the song card
         cy.get('.song__card__btn').should('be.visible')
         .contains('Деталі')
         .click()
-
+        cy.wait(300)
         cy.get('.info__btn__more__detail').should('be.visible')
         .contains('Більше деталей')
         .click()
-
         cy.get('.song-page__text-title').should('be.visible')
         .contains('Зозуленька')
+// Check the player
+        cy.get('button[class="btn__player play ng-star-inserted"]').should('be.visible')
+        // .click()
+        // cy.wait(1000)
+        // cy.get('button[class="btn__player pause ng-star-inserted"]').should('be.visible')
+        // .click()
     });
 })
