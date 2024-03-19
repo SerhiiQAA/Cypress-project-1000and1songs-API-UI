d987ffdd663ae0f94bd939c48743feca4a8fefb6
SerhiiQA <nabukve@gmail.com>
2024-03-19 21:08:50 +0200

    new

---
+++ cypress/e2e/Front/Filter_Map_page.cy.js
@@ -0,0 +1,32 @@
+describe('Support', () => {
+    it('Support', () => {
+        cy.viewport(1280, 1020)
+        cy.visit('https://1000and1songs.com/#/')
+        cy.get(':nth-child(4) > .menu-list__item-link').click()
+        cy.location('protocol').should('eq', 'https:')        
+// Filter button
+        cy.get('.filter__header').should('be.visible')
+        .contains('Фільтр')
+        .click()
+// Open filter
+        cy.get('.filter__body').should('be.visible')
+        .contains('Сховати фільтр')
+// Input data by song title
+        cy.get('#mat-input-1').should('be.visible')
+        .type('Зозуленька{enter}')
+// Check the search by song title
+        cy.get('.song__card__header').should('be.visible')
+        .contains('Зозуленька')
+
+        cy.get('.song__card__btn').should('be.visible')
+        .contains('Деталі')
+        .click()
+
+        cy.get('.info__btn__more__detail').should('be.visible')
+        .contains('Більше деталей')
+        .click()
+
+        cy.get('.song-page__text-title').should('be.visible')
+        .contains('Зозуленька')
+    });
+})
