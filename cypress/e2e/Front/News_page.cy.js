dd5952b239ca1f4feb894a968fde0a3f2291175d
SerhiiQA <nabukve@gmail.com>
2024-04-27 17:02:23 +0300

    new

--- cypress/e2e/Front/News_page.cy.js
+++ cypress/e2e/Front/News_page.cy.js
@@ -1,33 +1,19 @@
+import NewsP from "../pages/NewsPage";
+
 describe('News page', () => {
     it('News page', () => {
         cy.viewport(1280, 1020)
         cy.visit('/')
-        cy.get(':nth-child(5) > .menu-list__item-link').click()
-        cy.location('protocol').should('eq', 'https:')
-        cy.url().should('include', 'news')
-        // Header
-        cy.get('.header').should('be.visible')
-        // Footer
-        cy.get('.footer').should('be.visible')
-        //News container  
-        cy.get('.news').should('be.visible')
-        cy.contains('.news', 'Новини').should('be.visible');
-        // News_filter
-        cy.get('.news__filter').should('be.visible')
-        // Buttons filter
-        const buttonNames = ['Усі', 'Зустрічі', 'Лекції', 'Публікації', 'Майстер-класи', 'Концерти', 'Конференції'];
-        buttonNames.forEach(buttonName => {
-            cy.get('.news__filter').should('contain.text', buttonName);
-        });
-        // News_card
-        cy.get('.card__body')
-            .find('.card__image-item, .card__title-name, .card__title-place')
-            .should('be.visible');
-        // Cards count
-        cy.get('.cards__wrap')
-            .find('.card')
-            // .should('have.length', 2);
-        // Pagination
-        // cy.get('.pagination').should('be.visible')
+
+        const News = new NewsP()
+
+        News.validateNewsBtn()
+        News.validateHeaderContainer()
+        News.validateFooterContainer()
+        News.validateNewsContainer()
+        News.validateNewsFilter()
+        News.validateBtnFilter()
+        News.validateNewsCard()
+        News.validateCardsCount()
     });
 })
---
+++ cypress/e2e/pages/NewsPage.js
@@ -0,0 +1,36 @@
+class NewsP {
+    validateNewsBtn() {
+        cy.get(':nth-child(5) > .menu-list__item-link').click()
+        cy.location('protocol').should('eq', 'https:')
+        cy.url().should('include', 'news')
+    }
+    validateHeaderContainer() {
+        cy.get('.header').should('be.visible')
+    }
+    validateFooterContainer() {
+        cy.get('.footer').should('be.visible')
+    }
+    validateNewsContainer() {
+        cy.get('.news').should('be.visible')
+        cy.contains('.news', 'Новини').should('be.visible');
+    }
+    validateNewsFilter() {
+        cy.get('.news__filter').should('be.visible')
+    }
+    validateBtnFilter() {
+        const buttonNames = ['Усі', 'Зустрічі', 'Лекції', 'Публікації', 'Майстер-класи', 'Концерти', 'Конференції'];
+        buttonNames.forEach(buttonName => {
+            cy.get('.news__filter').should('contain.text', buttonName);
+        })
+    }
+    validateNewsCard() {
+        cy.get('.card__body')
+            .find('.card__image-item, .card__title-name, .card__title-place')
+            .should('be.visible');
+    }
+    validateCardsCount() {
+        cy.get('.cards__wrap')
+            .find('.card')
+    }
+}
+export default NewsP;
